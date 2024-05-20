18af284ec9a7e6311f079120cafb9871d0b91391
EngelsmaEelke <elengelsma@gmail.com>
2024-05-20 14:14:30 +0200

    Update database

--- BlazorBrandstof/SQL/CreateDatabase.sql
+++ BlazorBrandstof/SQL/CreateDatabase.sql
@@ -14,9 +14,8 @@ CREATE TABLE waiter
 
 CREATE TABLE categories 
 (
-    Id  int AUTO_INCREMENT NOT NULL,
     Name nvarchar(255),
-    PRIMARY KEY (Id)
+    PRIMARY KEY (Name)
 );
 
 CREATE TABLE products
@@ -99,12 +98,6 @@ INSERT INTO categories (Name)
 VALUES ('Lunch');
 
 INSERT INTO categories (Name)
-VALUES ('Catering drinks');
-
-INSERT INTO categories (Name)
-VALUES ('Catering food');
-
-INSERT INTO categories (Name)
 VALUES ('Re-usables / disposables');
 
 INSERT INTO products (Name, Price, Category) 
@@ -125,6 +118,135 @@ VALUES ('Espresso', '1.22', 'Warm drinks');
 INSERT INTO products (Name, Price, Category)
 VALUES ('Double Espresso', '1.88', 'Warm drinks');
 
+INSERT INTO products (Name, Price, Category)
+VALUES ('Latte Macchiato', '1.50', 'Warm drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Latte Macchiato large', '2.16', 'Warm drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Flat White', '1.88', 'Warm drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Tea', '1.17', 'Warm drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Fresh Tea', '1.30', 'Warm drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Hot Chocolate Dark', '2.10', 'Warm drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Chai Latte', '2.60', 'Warm drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Dirty Chai Latte', '3.25', 'Warm drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Coffee Special', '3.00', 'Warm drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Smoothie', '2.50', 'Cold drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Homemade Ice Tea', '2.75', 'Cold drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Water Still', '2.10', 'Cold drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Water Sparkling', '2.10', 'Cold drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Apple Juice', '2.30', 'Cold drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Lemon Lime Soda', '2.75', 'Cold drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Ice Latte', '2.70', 'Cold drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Tonic', '3.20', 'Cold drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Fritz Cola', '2.60', 'Cold drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Fritz Rhubarb', '2.60', 'Cold drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Fritz Limo Sinas', '2.60', 'Cold drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Fritz Lemon', '2.60', 'Cold drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Fritz Apple', '2.60', 'Cold drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Virgin Gin Tonic', '5.50', 'Alcohol free drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Van de streek IPA', '4.40', 'Alcohol free drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Nuchtere heit', '3.90', 'Alcohol free drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Heineken draft 0,0%', '3.20', 'Alcohol free drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Mocktail of the day', '5.50', 'Alcohol free drinks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Pie of the day', '2.25', 'Pastry\'s and dessert');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Fluffy pancakes', '6.00', 'Pastry\'s and dessert');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Bitterballen P.P', '1.10', 'Snacks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Roasted potato wedges', '4.00', 'Snacks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Vegetarian springroll', '4.50', 'Snacks');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Soup of the Day', '4.50', 'Lunch');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('12-Uurtje', '6.60', 'Lunch');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Croquette on sourdough bread', '6.50', 'Lunch');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Wrap Crispy Chicken', '7.30', 'Lunch');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Pot of the Season', '7.20', 'Lunch');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Grandma\'s meatball', '6.20', 'Lunch');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('LBS Circulware cup', '2.00', 'Re-usables / disposables');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('LBS Circulware lid', '2.00', 'Re-usables / disposables');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Re-usable Cutlery set', '4.50', 'Re-usables / disposables');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Wentelteefjes', '5.10', 'Lunch');
+
+INSERT INTO products (Name, Price, Category)
+VALUES ('Pasta Pesto', '5.85', 'Lunch');
+
 INSERT INTO tables (Name)
 VALUES ('table1');
 
@@ -133,3 +255,24 @@ VALUES ('table2');
 
 INSERT INTO tables (Name)
 VALUES ('table3');
+
+INSERT INTO tables (Name)
+VALUES ('table4');
+
+INSERT INTO tables (Name)
+VALUES ('table5');
+
+INSERT INTO tables (Name)
+VALUES ('table6');
+
+INSERT INTO tables (Name)
+VALUES ('table7');
+
+INSERT INTO tables (Name)
+VALUES ('table8');
+
+INSERT INTO tables (Name)
+VALUES ('table9');
+
+INSERT INTO tables (Name)
+VALUES ('table10');
