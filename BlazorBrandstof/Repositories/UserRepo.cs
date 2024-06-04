764f0a4b540554525131b78d66efbdcb77199a26
Onurcan37 <gozuacik3737@hotmail.com>
2024-06-04 11:26:36 +0200

    Login Pagina
    Login pagina is gemaakt en verbonden met de Database. Bij correct invoer verwijst de inlog pagina door naar de Home pagina.

---
+++ BlazorBrandstof/Repositories/UserRepo.cs
@@ -0,0 +1,27 @@
+﻿using BlazorBrandstof.Models;
+using Dapper;
+using MySqlConnector;
+
+namespace BlazorBrandstof.Repositories;
+
+public class UserRepo
+{
+    private static readonly string ConnectionString =
+        ConfigurationHelper.Configuration.GetConnectionString("brandstof")!;
+    
+    public static bool Verify(string username, string password)
+    {
+        // maak verbinding met database
+        using var connection = new MySqlConnection(ConnectionString);
+        
+        const string sql = "select * FROM users WHERE UserName = @UserName AND PassWord = @PassWord";
+        
+        var result = connection.Query(sql, new
+        {
+            UserName = username,
+            PassWord = password,
+        }).Count() > 0;
+
+        return result;
+    }
+}
